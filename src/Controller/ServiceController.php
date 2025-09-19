b1d59756efd73b6919fde0a14b044a9e94dc3e75
SENDA2004 <senda.smin@esprit.tn>
2025-09-19 19:45:39 +0200

    web 2.0 First Project

---
+++ src/Controller/ServiceController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ServiceController extends AbstractController
+{
+    
+    #[Route('/service/{name}', name: 'app_service_show')]
+    public function showService(string $name): Response
+    {
+        return $this->render('service/showService.html.twig', [
+            'name' => $name,
+        ]);
+    }
+
+    
+    #[Route('/go-to-index', name: 'app_go_to_index')]
+    public function goToIndex(): Response
+    {
+        return $this->redirectToRoute('app_home');
+    }
+}
